1de12e21dc0a7c235ab32998ad60316b51324482
Bigizic <olalekanisaac75@gmail.com>
2023-02-17 14:50:07 -0800

    bad line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* header */
+
+/**
+* main - Determine the alphabets in lower case.
+* Return: 0 if success.
+(*
+*/
+
+int main(void)
+{
+putchar("abcdefghijklmnopqrstuvwxyz\n");
+return (0);
+}
