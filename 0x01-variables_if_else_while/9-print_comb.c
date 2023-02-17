387d13b4fc243e06020816706debb207deeafdea
Bigizic <olalekanisaac75@gmail.com>
2023-02-17 15:59:41 -0800

    ffff

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*Header*/
+
+/**
+* main - entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int n;
+
+n = 48;
+
+while (n <= 57) /*57 corresponds with ASCII character 9*/
+{
+putchar (n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar ('\n'); /*add new line*/
+return (0);
+}
