6807dcb6d57ee2206162abd9d0c84eb986bcb367
Bigizic <olalekanisaac75@gmail.com>
2023-02-21 17:16:40 -0800

    one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+/**
+* main - Print "_putchar" followed by a new line.
+* Return: 0
+*/
+
+int main(void)
+{
+char ink  = "_putchar";
+int i = 0;
+while (ink[i] != '\0')
+{
+_putchar(ink[i]);
+i++;
+}
+_putchar('\n');
+
+return (0);
+}
