8ab02d1b18de9f591ebe7bd6c70b9a0c7101492e
Bigizic <olalekanisaac75@gmail.com>
2023-02-21 18:50:27 -0800

    add

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,16 @@ int z; /* computed value */
 while (x < 10)
 {
 y = 0;
-while (y < 10)	{
+while (y < 10)
+{
 z = x * y;
 
 if (z > 9)
 {
 _putchar(z / 10 + '0');
 _putchar(z % 10 + '0');
-}else if (y != 0)
+}
+else if (y != 0)
 {
 _putchar(' ');
 _putchar(z + '0');	}
