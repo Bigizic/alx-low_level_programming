375005d1e4bc3641d51f5318e6bd0c8472a2b464
Bigizic <olalekanisaac75@gmail.com>
2023-02-21 17:35:34 -0800

    updated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*header */
+/**
+* print_alphabet_x10 - Print alphabet in lowercase 10 times..
+* Return: 0 if success.
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+char c;
+
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
+}
