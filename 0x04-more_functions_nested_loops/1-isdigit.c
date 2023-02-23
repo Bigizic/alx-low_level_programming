598787a7bcdab5ccc6d6a348a5fbdca92d303e75
Bigizic <olalekanisaac75@gmail.com>
2023-02-23 15:51:13 -0800

    files

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,15 @@
 /*header*/
 
 /**
-* int _isdigit - checks for digits 0 through 9 ..
+* _isdigit - checks for digits 0 through 9 ..
 * @c: stores digit 0 to 9 ..
 * Return: 1 if c is a digit otherwise 0 ..
 */
 
 int _isdigit(int c)
 {
-if ( c >= 0 && c <= 9)
+if (c >= 0 && c <= 9)
 return (1);
-else 
+else
 return (0);
 }
