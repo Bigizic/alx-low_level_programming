e0bd16331bf21b8830082f1c320613cc9ba8b10f
Bigizic <olalekanisaac75@gmail.com>
2023-02-23 16:17:58 -0800

    files

---
+++ 0x04-more_functions_nested_loops/3-prints_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*header*/
+
+/**
+* print_numbers - prints the numbers from 0 to 9...
+* Return: 0 if success..
+*/
+
+void print_numbers(void)
+{
+for (int x = 0; x<10; x++)
+{
+_putchar ("%d", x);
+}
+_putchar('\n');
+return (0);
+}
