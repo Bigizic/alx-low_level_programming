ca8c8c0fa696f562e76c2aa20059c88753167a0c
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 13:54:29 -0800

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+
+#endif /* MAIN_H */
