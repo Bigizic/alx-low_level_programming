504e52ffd5d86c10df72d6fffc3d18d40f03fee6
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 05:34:55 -0800

    new

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*header*/
+
+/**
+* print_times_table - prints a table starting with 0.
+* @n: contains digits.
+* Return: nothing .
+*/
+
+void print_times_table(int n)
+{
+if (n > '15' || n < '0')
+return;
+}
