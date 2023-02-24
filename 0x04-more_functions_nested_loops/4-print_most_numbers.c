28bf2597aa93f5353b54d70504fe1d7fa2fba50a
Bigizic <olalekanisaac75@gmail.com>
2023-02-23 17:30:27 -0800

    new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
 *print_most_numbers- prints the numbers, from 0 to 9 within 2 and 4
 *
-*Return: from 0 to 9 within 2 and 4, followed by a new line.
+*Return: from 0 to 9 fon't print 2 and 4, followed by a new line.
 **/
 void print_most_numbers(void)
 {
@@ -12,7 +12,14 @@ int i;
 for (i = 0; i <= 9; i++)
 {
 if (i != 2 && i != 4)
+{
 _putchar(i + '0');
+}
+else if (i == 2 && i == 4)
+{
+return;
+}
+
 }
 _putchar('\n');
 }
