22073f5e469263bd407ea4944ceb3be3066498ce
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 13:56:54 -0800

    gile

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
-
 /**
-* print_numbers - will print the numbers from 0 to 9
-*/
+*print_numbers- a function that prints the numbers, from 0 to 9
+*
+*Return: print to stdout the alphabet, in lowercase, followed by a new line.
+**/
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
-	_putchar(c);
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 
-#endif /* MAIN_H */
+#endif
