838a385c3d6a2d92ee77d015af1420a3c73a9b64
Bigizic <olalekanisaac75@gmail.com>
2023-02-23 16:02:26 -0800

    files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*header*/
+/**
+* mul - multiplies two integers ..
+* @a: contains integers a...
+* @b: contains integers b ..
+* Return: 0 if success ..
+*/
+
+int mul(int a, int b)
+{
+_putchar("%d" * "%d", a, b)
+return (0);
+}
