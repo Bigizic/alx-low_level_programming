466936e6a70423a5b49976eba5ccaf20ad3a8537
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 05:47:25 -0800

    g

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,25 +3,25 @@
 #include <math.h>
 /*HEADERS*/
 /**
-* main - main block
-* Description: Print the sum of all multiples of 3 or 5 below 1024.
-* Return: 0
+* main - main block..
+* Description: Print the sum of all multiples of 3 or 5 below 1024 ..
+* Return: 0 if success..
 */
 int main(void)
 {
 int i = 1;
-int total = 0;
+int t = 0;
 
 while (i < 1024)
 {
 if (i % 3 == 0)
-otal += i;
+t += i;
 else if (i % 5 == 0)
-total += i;
+t += i;
 
 i++;
 }
-printf("%d\n", total);
+printf("%d\n", t);
 
 return (0);
 }
