b20229d65781aa1b18ff9ee0da87c3cae15e204c
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 14:30:58 -0800

    fgt

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagonal - prints diagonal line on the terminal
+*@n: numbers of lines and columns
+* Return: none
+**/
+
+void print_diagonal(int n)
+{
+int l, c;
+
+for (l = 0; l < n; l++)
+{
+for (c = 0; c <= l; c++)
+{
+if (c != l)
+{
+putchar(' ');
+}
+else
+{
+putchar('\\');
+}
+
+}
+putchar('\n');
+}
+if (n <= 0)
+putchar('\n');
+}
