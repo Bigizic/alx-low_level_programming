ba06926fa536f5115f37f0795575e805dd0eaaff
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 16:38:24 -0800

    gh

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* print_number - print an int numbers.
-* @n: number tested
-* Return: Always 0.
-*/
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
+ */
 void print_number(int n)
 {
-int i, j, digit, digits, power;
-unsigned int temp, numchar, number;
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
 
-digit = 0;
-if (n < 0)
-{
-putchar('-');
-temp = -n;
-}
-else
-{
-temp = n;
-}
+	digit = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	else
+	{
+		temp = n;
+	}
 
-number = temp;
+	number = temp;
 
-while (number >= 10)
-{
-number = number / 10;
-digit++;
-}
-digits = digit + 1;
-power = 1;
-i = 1;
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
 
-while (i < digits)
-{
-power = power * 10;
-i++;
-}
-j = power;
-while (j >= 1)
-{
-numchar = (temp / j) % 10;
-putchar(numchar + '0');
-j = j / 10;
-}
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
+	}
 }
