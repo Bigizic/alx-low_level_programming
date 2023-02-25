0b64501d7b1f6e66e2130cbb20ea9372c2e49cdb
Bigizic <olalekanisaac75@gmail.com>
2023-02-24 17:14:41 -0800

    int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,25 +9,7 @@
 
 void print_number(int n)
 {
-
-if (n < 0) 
-{
-putchar('-');
-n = -n;
+putchar("%d", n)
+return 0;
 }
 
-if (n == 0)
-putchar('0');
-
-if (n/10)
-printnumber(n/10);
-
-putchar(n%10 + '0');
-}
-
-int main(int argc, char** argv)
-{
-int n = atoi(argv[1]);
-printnumber(n);
-printf("\n");
-}
