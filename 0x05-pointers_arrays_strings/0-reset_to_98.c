b8c527af83bc77c8633ebbb91cbc84002197028e
Bigizic <olalekanisaac75@gmail.com>
2023-02-27 03:14:51 -0800

    m

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,14 +4,13 @@
 
 /**
 * reset_to_98 - updates the value of in to 98 ..
+* @n: take an int type ..
 * Return: 0 if success ..
 */
 
 void reset_to_98(int *n)
 {
-*n = '98';
-
-putchar(%x\n", n);
+*n = 98;
 
 return;
 
