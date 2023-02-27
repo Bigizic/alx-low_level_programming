e488099ee950c09ad2eef4dcc32ec0d26b8d1c30
Bigizic <olalekanisaac75@gmail.com>
2023-02-27 08:07:43 -0800

    low

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,19 +3,45 @@
 /*header*/
 
 /**
-* puts_half - prints half of a string ..
-* @str: char type .
+* _strlen - prints half of a string ..
+* @s: char type .
 * Return: 0 if success ..
 */
 
-void puts_half(char *str)
+void _strlen(char *s)
 {
 int w, i =0;
 
-while (str[i] != '\0')
+while (s[i] != '\0')
 {
 i++;
 }
 w = i;
 return (1);
+}
+
+/**
+* puts_half -  prints half of a string
+* @str: a pointer to string
+* Return: string
+**/
+
+void puts_half(char *str)
+{
+int i;
+int man = _strlen(str);
+
+if (man % 2 != 0)
+{
+i = (man / 2) + 1;
+}
+else
+{
+i = man / 2;
+}
+for (i = i; i < man; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* print_array - prints n elements of an array of integers ..
+* @a: int type .
+* @n: int type ..
+* Return: 0 if success ..
+*/
+
+void print_array(int *a, int n)
+{
+int w;
+
+for (w = 0; w < n; w++)
+{
+if (w != n -1)
+{
+printf("%d", a[w]);
+}
+else
+{
+printf("%d", a[w]);
+}
+}
+printf("\n");
+}
