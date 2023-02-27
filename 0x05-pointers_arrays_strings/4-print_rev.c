6c01c76d270f4ec62cf4b2e6dbc3b4b2e85a30d1
Bigizic <olalekanisaac75@gmail.com>
2023-02-27 07:19:38 -0800

    files

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <Stdio.h>
+#include <stdio.h>
 /*header*/
 
 /**
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* rev_string - reverses a string ..
+* @s: char type ..
+* Return: 0 if success ..
+*/
+
+void rev_string(char *s)
+{
+
+int i;
+char c;
+
+for (i = 0; s[i] != 0; i++)
+{
+}
+
+for (i = i -1; i >= 0; i--)
+{
+c = s[i];
+}
+
+return;
+}
+
