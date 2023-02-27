5f8276fe86820091588c5a92d0ac18ae6278ccb0
Bigizic <olalekanisaac75@gmail.com>
2023-02-27 03:24:47 -0800

    f

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* swap_int - swaps the value of two integers ..
+* @a: type int ..
+* @b: type int ..
+* Return: 0 if success ..
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+
+return;
+}
