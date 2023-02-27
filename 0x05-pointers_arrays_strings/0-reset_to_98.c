f7e653eb3ff16802a1b71a4138d5e164d3b0b20d
Bigizic <olalekanisaac75@gmail.com>
2023-02-27 02:54:52 -0800

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* reset_to_98 - updates the value of in to 98 ..
+* Return: 0 if success ..
+*/
+
+void reset_to_98(int *n)
+{
+*n = '98';
+
+putchar(%x\n", n);
+
+return;
+
+}
