d5a623ee70fab1033b03e9eae9016959d53ef556
Bigizic <olalekanisaac75@gmail.com>
2023-02-27 03:47:42 -0800

    files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* _strlen - retrns the length of a string ..
+* @s; char type ..
+* Return: length of string if success ..
+*/
+
+int _strlen(char *s)
+{
+int c = strlen(*s);
+
+return (c);
+}
