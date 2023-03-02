4e8fec001d15a7144d794f4bb9828f7e783ef386
Bigizic <olalekanisaac75@gmail.com>
2023-03-01 16:02:57 -0800

    files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* _strcmp - compares two strings ..
+* @s1: char type ..
+* @s2: char type ..
+* Return: void ..
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int cd;
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+}
+for (cd = 0; s2[cd] != '\0'; cd++)
+{
+}
+
+if (s1 < s2)
+return (-i + (-cd));
+else if (s2 < s1)
+return (i + cd);
+else
+return (0);
+}
