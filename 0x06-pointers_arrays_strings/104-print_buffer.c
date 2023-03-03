277601ff5d13d3c33737d7531057fcc0370a78e0
Bigizic <olalekanisaac75@gmail.com>
2023-03-02 21:01:10 -0800

    fies

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* print_buffer - prints a buffer ..
+* @b: char type...
+* @size: int type ..
+* Return: 0 if success .
+*/
+
+void print_buffer(char *b, int size)
+{
+
