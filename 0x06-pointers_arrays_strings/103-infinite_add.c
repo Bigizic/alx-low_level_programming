eadc7b46aebfa967d78f471a93cd5330039fdebf
Bigizic <olalekanisaac75@gmail.com>
2023-03-02 20:52:41 -0800

    file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,3 +2,15 @@
 #include <stdio.h>
 /*header*/
 
+/**
+* *infinite_add - adds two numbers ..
+* @n1: char type ..
+* @n2: char type ..
+* @r: char type ..
+* @size_r: int type ..
+* Return: 0 if success ..
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
