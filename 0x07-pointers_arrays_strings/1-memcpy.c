da0778c1960ed63f13786697558fe7d5e120b187
Bigizic <olalekanisaac75@gmail.com>
2023-03-06 16:58:46 -0800

    file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,2 +1,21 @@
 #include "main.h"
+#include <string.h>
+/*header*/
 
+/**
+* *_memcpy - copies memory area ..
+* @dest: char type ..
+* @src: char type..
+* @n: int type ..
+* Return: 0 if success .
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int i;
+
+for (i = 0; i < n; i++)
+*dest = src[i];
+return (dest);
+}
