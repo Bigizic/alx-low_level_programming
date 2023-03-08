4f80e5ea0b72bb6fc5c4815831fbff18393a12d1
Bigizic <olalekanisaac75@gmail.com>
2023-03-08 10:26:10 -0800

    file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* _puts_recursion - prints a string ..
+* @s: char type ..
+* Return 0 if success ..
+*/
+
+void _puts_recursion(char *s)
+{
+putchar(*s);
+putchar('\n');
+}
