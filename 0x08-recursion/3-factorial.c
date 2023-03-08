54ed2b39f40a31b9ee9afc67c22673e4c708ec84
Bigizic <olalekanisaac75@gmail.com>
2023-03-08 14:39:40 -0800

    file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,11 +9,14 @@
 */
 
 int factorial(int n)
-{	
-if (n < 0)
+{
+if (n == 0 || n == 1)
+{
+return (1);
+}
+else if (n < 0)
 {
 return (-1);
 }
-n= n * (n - 1);
-return (n);
+return (n * factorial(n - 1));
 }
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <Stdio.h>
+/*header*/
+
+/**
+* _pow_recursion - returns the value of x raised to the power of y .. 
+* @x: int type ..
+* @y: int type ..
+* Return: 0 if success ..
+*/
+
+int _pow_recursion(int x, int y)
+{
+
