3672f37c7012b3572d2f6372782aef288758d149
Bigizic <olalekanisaac75@gmail.com>
2023-03-11 12:50:15 -0800

    file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* main - prints it's name ..
+* @argc: int type ..
+* @argv: char type ..
+* Return: 0 if success ..
+*/
+
+int main(int argc, char* argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
