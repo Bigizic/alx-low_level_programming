e482f79e250a973551c7b3c93e903c132e95cef1
Bigizic <olalekanisaac75@gmail.com>
2023-03-12 04:44:45 -0700

    file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* main - multiplies two numbers ..
+* @argc: int type ..
+* @argv: char type ..
+* Return: 0 if success ..
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+int j = 1;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+j *= atoi(argv[i]);
+printf("%d\n", j);
+return (0);
+}
