349f1ee1a8a6ab103c64e2b6d4fdd3282f355f6c
Bigizic <olalekanisaac75@gmail.com>
2023-03-20 21:42:33 -0700

    file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,21 @@
 /*header */
 
 /**
-* 
-*
-*
+* print_dog - prints a struct dog ..
+* @d: pointer to my_dog ..
+* Return: 0 if success ..
 */
+
+void print_dog(struct dog *d)
+{
+
+if (d == NULL)
+{
+printf("nil");
+}
+if (name == NULL)
+{
+printf("Name: (nil)")
+}
+
+}
