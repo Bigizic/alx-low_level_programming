4bc6d0385f88b2e124cb650cad4cb83600b165a1
Bigizic <olalekanisaac75@gmail.com>
2023-03-20 21:43:38 -0700

    file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,8 +11,8 @@ struct dog
 char *name;
 float age;
 char *owner;
-}dog_t;
-typedef struct dog dog;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
