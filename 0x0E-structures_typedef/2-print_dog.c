b48a7a1dce7c1e45671f6f385b915602abc30921
Bigizic <olalekanisaac75@gmail.com>
2023-03-20 23:32:43 -0700

    file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 if (d)
 {
 d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
-printf("Age: %.1f\n", d->age);
+printf("Age: %f\n", d->age);
 d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 }
 }
