b0225f763d7fa6ff3d3f2adfd8243997819384ee
Bigizic <olalekanisaac75@gmail.com>
2023-03-22 14:46:46 -0700

    file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/*header*/
+
+/**
+* print_name - prints a name ..
+* @name: char pointer ..
+* @f: void pointer ..
+* Return: 0 if success ..
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+return;
+}
