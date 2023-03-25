de020ed3feae58ac2e2132b6f640fa3961fe1d5b
Bigizic <olalekanisaac75@gmail.com>
2023-03-24 18:29:05 -0700

    file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ unsigned int i;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-const char* str = va_arg(args, const char*);
+const char *str = va_arg(args, const char*);
 if (str == NULL)
 {
 printf("(nil)");
