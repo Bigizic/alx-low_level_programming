b1347914e9909d5d0faee3ce0e66120167af8795
Bigizic <olalekanisaac75@gmail.com>
2023-03-24 18:54:24 -0700

    file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+typedef struct format
+{
+char *f;
+void (*func)(va_list vargs);
+}form;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
