a06d2e2743ef68252f44dd91ce6e0b7f02093283
Bigizic <olalekanisaac75@gmail.com>
2023-03-30 16:03:10 -0700

    file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,13 @@
 /* header */
 
 /**
+ * before_main - prints message before main
 * __attribute__ - tells compiler to execute code before main
 * Return: 0 if success
 */
 
 void __attribute__((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
