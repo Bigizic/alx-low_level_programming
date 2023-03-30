fc7903678094e9da2045f8985e74c63310a8b801
Bigizic <olalekanisaac75@gmail.com>
2023-03-30 15:11:55 -0700

    list header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,6 +21,7 @@ typedef struct list_s
 } list_t;
 
 /* functions */
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
