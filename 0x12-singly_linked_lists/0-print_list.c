4a52b1706cb89734f3cb781249e9759dc099879e
Bigizic <olalekanisaac75@gmail.com>
2023-03-30 14:54:24 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/* header */
+
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer to list_t
+* Return: 0 if success
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		
+			printf("[%d} %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
