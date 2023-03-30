5ac1f84c30be609d121f3acb2b1579399c4c6b68
Bigizic <olalekanisaac75@gmail.com>
2023-03-30 06:56:28 -0700

    file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,7 +6,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
-void condition(int argc, char *argv[], int i, int j);
-void print_number(int mul, int numD);
+void *condition(int argc, char *argv[], int i, int j);
+void *print_number(int mul, int numD);
 int main(int argc, char *argv[]);
 #endif
