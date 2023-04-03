a6b8573d1c7f660745cefc2a55189cda85ff8c61
Bigizic <olalekanisaac75@gmail.com>
2023-04-03 09:34:28 -0700

    frees a listint_t and set HEAD to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,15 +13,14 @@ void free_listint2(listint_t **head)
 
 	current_node = *head;
 
-	if (*head != NULL)
-	{
-		*head = NULL;
-	}
-
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
 		free(current_node);
 		current_node = next_node;
+	}
+	if (*head != NULL)
+	{
+		*head = NULL;
 	}
 }
