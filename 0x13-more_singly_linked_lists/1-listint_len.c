940296a38a22868cc10c023c10d3b7694c7871f6
Bigizic <olalekanisaac75@gmail.com>
2023-04-03 07:24:45 -0700

    return number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/* header */
+
+/**
+* listint_len -  returns the number of elements in a linked listint_t list.
+* @h: pointer to a const listint_t
+* @Return: 0 if success
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h != NULL)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
