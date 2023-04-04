44d9f157b46fcffb03947d90a19cf1fc5270fa19
Bigizic <olalekanisaac75@gmail.com>
2023-04-04 12:30:48 -0700

    prints linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,22 +9,32 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *current_node, *next_node;
+	const listint_t *current_node, *next_node;
 	size_t count = 0;
 
-	current_node = (listint_t *)head;
+	current_node = (listint_t *) head;
 
 	while (current_node != NULL)
 	{
-		printf("[%p] %d\n", (void *)current_node, current_node->n);
 		count++;
+		printf("[%p] %d\n", (void *)current_node, current_node->n);
 		next_node = current_node->next;
 
-		if (next_node <= current_node)
-		{
-			printf("-> [%p] %d\n", (void *)next_node, next_node->n);
-		}
+		if (next_node == NULL || next_node >= current_node)
+			break;
+
 		current_node = next_node;
+
+		if (current_node != NULL && next_node != NULL)
+		{
+			count++;
+
+			printf("[%p} %d\n", (void *)current_node, current_node->n);
+
+			printf("-> [%p] %d\n", (void *)next_node, next_node->n);
+
+		}
+
 	}
 	return (count);
 	exit(98);
