54ac129dc989b7a979e597b792401b598d69dbf1
Bigizic <olalekanisaac75@gmail.com>
2023-04-04 17:42:02 -0700

    header

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #define MAX_DIGITS 10
-
 #include "ut.h"
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -10,5 +10,8 @@ int *array_range(int min, int max);
 int _putchar(char c);
 void condition(int argc, char *argv[], int i, int j);
 void print_number(int mul, int numD);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+int get_digit(char c);
 int main(int argc, char *argv[]);
 #endif
