8a8eaefc778151a761bac2a951a8db4aa901df9f
Bigizic <olalekanisaac75@gmail.com>
2023-04-06 14:37:33 -0700

    ls

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* header */
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+* @filename: const char
+* @letters: size_t type
+* Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
