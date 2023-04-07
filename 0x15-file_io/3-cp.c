3540ff93396466f9ca586dc937b8109d1b9b0c80
Bigizic <olalekanisaac75@gmail.com>
2023-04-07 12:44:04 -0700

    copy-file1-to-file2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,38 +15,64 @@ int two_files(char *file_from, char *file_to, size_t count)
 	int file2, i, g, j;
 	ssize_t print;
 	size_t read2;
-	char *buffer = malloc(count * sizeof(char));
+	char *buffer = malloc(count);
+
+	if (buffer == NULL)
+		return (-1);
 
 	file1 = fopen(file_from, "r");
-	read2 = fread(buffer, sizeof(char), count, file1);
-	if (file_from == NULL || read2 == -1)
+	if (file1 == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
+		free(buffer);
 		exit(98);
 	}
 	file2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (file_to != NULL)
+	if (file2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		fclose(file1);
+		free(buffer);
+		exit(99);
+	}
+	read2 = fread(buffer, sizeof(char), count, file1);
+	while (read2 > 0)
 	{
 		print = write(file2, buffer, read2);
-		if (file2 == -1 || print != 0)
+		if (print == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			fclose(file1);
+			close(file2);
+			free(buffer);
 			exit(99);
 		}
 	}
-	i = fclose(file1);
-	g = fileno(file1);
-	j = close(file2);
-	if (i != 0)
+
+	if (read2 == 0 && ferror(file1))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", g);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
+		fclose(file1);
+		close(file2);
+		free(buffer);
+		exit(98);
+	}
+	i = fclose(file1);
+	if (i == EOF)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", file_from);
+		close(file2);
+		free(buffer);
 		exit(100);
 	}
+	j = close(file2);
 	if (j == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file2);
+		free(buffer);
 		exit(100);
 	}
+	free(buffer);
 	return (0);
 }
 
