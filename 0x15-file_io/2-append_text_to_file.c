3d576457da1906fd1fea3217283546065e41868a
Bigizic <olalekanisaac75@gmail.com>
2023-04-07 08:12:53 -0700

    append-text-to-file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,39 +10,29 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	FILE *ptrf;
-	int exist;
-	size_t length = strlen(text_content);
-	size_t print;
+	int ptrf;
+	int length;
+	ssize_t print;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-	exist = access(filename, F_OK);
-	if (exist == -1)
-	{
+		ptrf = open(filename, O_WRONLY | O_APPEND);
+		if (ptrf == -1)
+		{
+			return (-1);
+		}
+		if (text_content != NULL)
+		{
+		length = strlen(text_content);
+		print = write(ptrf, text_content, length);
+		if (print != length)
+		{
+		close(ptrf);
 		return (-1);
-	}
-	ptrf = fopen(filename, "a");
-	if (ptrf == NULL)
-	{
-		return (-1);
-	}
-
-	if (text_content == NULL)
-	{
-		fclose(ptrf);
-		return (0);
-	}
-
-	print = fwrite(text_content, sizeof(char), length, ptrf);
-	fclose(ptrf);
-
-	if (print != length)
-	{
-		return (-1);
-	}
+		}
+		}
+	close(ptrf);
 	return (1);
 }
