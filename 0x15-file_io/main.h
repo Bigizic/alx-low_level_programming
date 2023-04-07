41d94f6d707fa321ada5f24b06add2ece103767c
Bigizic <olalekanisaac75@gmail.com>
2023-04-07 13:47:55 -0700

    heeader

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int two_files(char *file_from, char *file_to, size_t count);
-int read2fun(size_t *read2, FILE **file1, int *file2, char **buffer);
+int read2fun(size_t *read2, FILE **file1, int *file2, char **buffer, char *file_from);
 int fd_value(int *i, int *j, char **file_from, int *file2, char **buffer);
 int main(int argc, char *argv[]);
 #endif
