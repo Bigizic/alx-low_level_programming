7dcb7fb6b5bec9780c8f422399a91d770d3a3856
Bigizic <olalekanisaac75@gmail.com>
2023-04-08 08:45:13 -0700

    copy-file1-to-file2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,96 +2,102 @@
 /* header */
 
 /**
-* checkRead - function for read 2
-* @r2: int type
-* @f1: int type
-* @ffrom: char pointer to a pointer
-* @f2: int type
-* Return: 0 if success
-*/
-
-int checkRead(int *r2, int *f1, char **ffrom, int *f2)
-{
-	if (*r2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *ffrom);
-		exit(98);
-	}
-
-	if (close(*f1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close  %d\n", *f1);
-		exit(100);
-	}
-
-	if (close(*f2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close  %d\n", *f2);
-		exit(100);
-	}
-	return (0);
-
-}
-
-
-/**
-* two_files - contains two files and run the program
-* @file_from: char pointer to first file
-* @file_to: char pointer to second file
-* Return: a program that read onef and write to twof
-*/
-
-int two_files(char *file_from, char *file_to)
-{
-	int file1, file2, print, read2;
-	char buffer[BUFFER];
-
-	file1 = open(file_from, O_RDONLY);
-	if (file1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
-		exit(98);
-	}
-
-	file2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC);
-	fchmod(file2, 0664);
-	if (file2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	read2 = read(file1, buffer, BUFFER);
-	if (read2 > 0)
-	{
-		print = write(file2, buffer, read2);
-		if (print != read2)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-	checkRead(&read2, &file1, &file_from, &file2);
-	return (0);
-}
-
-
-/**
-* main - contain filenames and their length
-* @argc: int type
-* @argv: char type
-* Return: copied file
+* main - contains two files and run the program
+* @argc: int type, also points to the length of arguments passed
+* @argv: char pointer to file names
+* Return: a program that read firstFile and write to secondFile
 */
 
 int main(int argc, char *argv[])
 {
+	int file1, file2, print, read2;
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = malloc(BUFFER * sizeof(char));
+	if (buffer == NULL)
+		return (-1);
 
-	two_files(argv[1], argv[2]);
+	file1 = open(argv[1], O_RDONLY);
+	file1Error(file1, buffer, argv[1]);
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
+	fchmod(file2, 0664);
+	file2Error(file2, buffer, argv[2]);
+	do {
+		read2 = read(file1, buffer, BUFFER);
 
+		if (read2 ==  0)
+			break;
+		file1Error(read2, buffer, argv[1]);
+		print = write(file2, buffer, read2);
+		file2Error(print, buffer, argv[2]);
+	} while (print >= BUFFER);
+	read2 = close(file2);
+	closeError(read2, buffer);
+	read2 = close(file1);
+	closeError(read2, buffer);
+	free(buffer);
 	return (0);
+}
+
+
+/**
+* file1Error - handle error 98
+* @f1: int type
+* @buff: char pointer
+* @av: char pointer
+* description: error message
+*/
+
+void file1Error(int f1, char *buff, char *av)
+{
+
+	if (f1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av);
+		free(buff);
+		exit(98);
+	}
+
+}
+
+
+/**
+* file2Error - handles error 99
+* @f2: int type
+* @buff: char pointer
+* @av: char pointer
+* description: error message
+*/
+
+void file2Error(int f2, char *buff, char *av)
+{
+	if (f2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av);
+		free(buff);
+		exit(99);
+	}
+}
+
+
+/**
+* closeError - handle error 100
+* @f2: int type
+* @buff: char pointer
+* description: error message
+*/
+
+void closeError(int f2, char *buff)
+{
+	if (f2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", f2);
+		free(buff);
+		exit(100);
+	}
 }
