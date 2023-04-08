7bf6315e6824575956cec12d3f0dd631e2078e32
Bigizic <olalekanisaac75@gmail.com>
2023-04-08 07:28:05 -0700

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +11,7 @@
 #include <ctype.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
 
 /* headers */
 
@@ -17,9 +20,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int checkRead(size_t *r2, FILE **f1, char **fr, int *f2, char **buff);
-int closeFiles(FILE **f1, char **fr, int *f2, char **buff);
-int two_files(char *file_from, char *file_to, size_t count);
+int checkRead(int *r2, int *f1, char **ffrom, int *f2);
+int two_files(char *file_from, char *file_to);
 int main(int argc, char *argv[]);
 
 #endif
