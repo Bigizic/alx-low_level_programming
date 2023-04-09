727989da2f6043d7776d430e2023fc6511462c72
Bigizic <olalekanisaac75@gmail.com>
2023-04-09 13:54:16 -0700

    set-bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,10 @@
 * @index: unisgned int
 *
 * Description: this function gets the value of a bit
-* at a given index and set it's value to 1
+* at a given index using the or operator (|) and set
+* it's value to 1 using the << (bit shifting) operator
+* to shift the bits of the left hand operator to the left
+* by a number of positions.
 *
 * Return: 1 or -1 if not
 */
