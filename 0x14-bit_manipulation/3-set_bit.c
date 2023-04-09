a4c97be040a0c5f38f8877c6e7fed70aa663650d
Bigizic <olalekanisaac75@gmail.com>
2023-04-09 13:28:50 -0700

    set-bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -17,12 +17,12 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 
-	if (index >= 64)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
 
 	*n |= (1UL << index);
-	return (*n);
+	return (1);
 }
