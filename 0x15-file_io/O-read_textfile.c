75a6f1a99762d43641b00a2ef53b77834a207421
Bigizic <117412923+Bigizic@users.noreply.github.com>
2023-05-05 05:17:57 +0100

    read_textfile

---
+++ 0x15-file_io/O-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/* header */
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+* @filename: const char
+* @letters: size_t type
+* Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr = fopen(filename, "r");
+	char *buffer = malloc(letters * sizeof(char));
+	size_t read;
+	ssize_t print;
+
+	if (filename == NULL || fptr == NULL)
+	{
+		return (0);
+	}
+
+	read = fread(buffer, sizeof(char), letters, fptr);
+	print = write(STDOUT_FILENO, buffer, read);
+
+	if (print < 0 || (size_t)print != read)
+	{
+		return (0);
+	}
+	fclose(fptr);
+	return (read);
+}
