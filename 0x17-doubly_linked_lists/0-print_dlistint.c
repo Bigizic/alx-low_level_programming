cec276b75b721e3d1c60db91b33efc082d47232b
Bigizic <olalekanisaac75@gmail.com>
2023-05-18 21:01:32 +0000

    Update print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,11 +2,11 @@
 /* header */
 
 /**
- * print_dlistint - a function that prints all the elements of a dlistint_t list.
- * @h: pointer to header of double linked list
- *
- * Return: the number of nodes
- */
+* print_dlistint - a function that prints all the elements of a dlistint_t list.
+* @h: pointer to header of double linked list
+*
+* Return: the number of nodes
+*/
 
 size_t print_dlistint(const dlistint_t *h)
 {
