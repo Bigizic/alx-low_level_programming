565e042c460feb764cdd8bd4ec21e00700316461
Bigizic <olalekanisaac75@gmail.com>
2023-05-19 11:51:25 +0000

    Update get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/* header */
+
+/**
+* get_dnodeint_at_index - a function that returns the nth node
+* of a dlistint_t linked list
+*
+* @head: pointer to the list
+*
+* @index: index to return
+*
+* Return: the node of index else NULL
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *head_copy = head, *new_node;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	while (head_copy != NULL)
+	{
+		head_copy = head_copy->next;
+		count++;
+		if (index == count)
+		{
+			new_node->n = head_copy->n;
+		}
+	}
+	if (index > count)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
+}
