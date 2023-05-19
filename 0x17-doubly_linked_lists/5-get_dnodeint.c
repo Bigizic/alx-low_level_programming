3f6605547795a6c2b285cf4fb1340e16973f9be6
Bigizic <olalekanisaac75@gmail.com>
2023-05-19 12:38:21 +0000

    Update get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,31 +14,18 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *head_copy = head, *new_node;
+	dlistint_t *head_copy = head;
 	unsigned int count = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	if (index == 0)
-	{
-		new_node->n = head_copy->n;
-		return (new_node);
-	}
 	while (head_copy != NULL)
 	{
+		if (count == index)
+		{
+			return (head_copy);
+		}
 		head_copy = head_copy->next;
 		count++;
-		if (index == count)
-		{
-			new_node->n = head_copy->n;
-		}
 	}
-	if (index > count)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	return (new_node);
+
+	return (NULL);
 }
