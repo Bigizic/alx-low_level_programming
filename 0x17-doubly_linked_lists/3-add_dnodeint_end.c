2a07d445888f217a1816efe2a89693e559e05324
Bigizic <olalekanisaac75@gmail.com>
2023-05-19 09:57:51 +0000

    Update add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new_node;
 	else
 	{
-		while (current_node->next != NULL)
+		while (current_node->next != NULL && current_node->n < n)
 			current_node = current_node->next;
 		current_node->next = new_node;
 	}
