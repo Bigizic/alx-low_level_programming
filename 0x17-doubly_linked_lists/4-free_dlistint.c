b12d45003ce645297f5798e06feb2f45805f50b8
Bigizic <olalekanisaac75@gmail.com>
2023-05-19 10:33:08 +0000

    Update free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/* header */
+
+/**
+* free_dlistint - a function that frees a dlistint_t list
+*
+* @head: pointer to list to free
+*
+* Return: void
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *head_copy;
+
+	while (head != NULL)
+	{
+		head_copy = head;
+		head = head->next;
+		free(head_copy);
+	}
+	return (0);
+}
