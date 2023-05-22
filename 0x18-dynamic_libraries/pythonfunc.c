bc5ccb53944a09a42ce96772da076744ed4a3e64
Bigizic <olalekanisaac75@gmail.com>
2023-05-22 22:29:55 +0000

    Add pythonfunc.c

---
+++ 0x18-dynamic_libraries/pythonfunc.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+/* header */
+
+/**
+* add - adds twp numbers
+* @a: int numbere to add
+* @b: int number to add
+*
+* Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * add - adds twp numbers
+ * @a: int number
+ * @b: int number
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * add - adds twp numbers
+ * @a: int number
+ * @b: int number
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * add - adds twp numbers
+ * @a: int number
+ * @b: int number
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a/b);
+}
+
+
+/**
+ * add - adds twp numbers
+ * @a: int number
+ * @b: int number
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
