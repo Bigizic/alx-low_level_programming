cc544e431023a505f82757f207adb2e239bb144d
Bigizic <olalekanisaac75@gmail.com>
2023-06-09 20:24:40 +0000

    Update hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -50,7 +50,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	/* no collision, set new_element as first node */
-	ht->array[index] = new_element;
 
 	return (1);
 }
