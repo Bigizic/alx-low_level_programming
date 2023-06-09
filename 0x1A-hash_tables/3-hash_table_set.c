e495419c1b9b372df6b276f4d9d62d2aae058c53
Bigizic <olalekanisaac75@gmail.com>
2023-06-09 13:43:55 +0000

    Update hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,6 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		/* Add the new_element to the beginning of the list */
 		new_element->next = ht->array[index];
+		ht->array[index] = new_element;
 	}
 
 	/* no collision, set new_element as first node */
