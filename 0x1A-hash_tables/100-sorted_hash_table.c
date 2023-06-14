d302f2c8cede47e537109863c4ad180d2df2f2b6
Bigizic <olalekanisaac75@gmail.com>
2023-06-14 15:17:05 +0000

    Update sorted_hash_table.c

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/* header */
+
+/**
+ * shash_table_create - a function that creates a hash table
+ *
+ * @size: (unsigned long int) size of the hash table
+ *
+ * Return: NULL if it can't be created
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+
