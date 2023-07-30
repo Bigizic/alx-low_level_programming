e4646d7d1ba56eeb6bb3f124c372b9e8e0aeda57
Bigizic <olalekanisaac75@gmail.com>
2023-07-30 14:02:53 +0000

    Add main

---
+++ 0x1C-makefiles/main/5-island_perimeter_alternative.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A Technical interview question
+"""
+
+def island_perimeter(grid):
+    """Created a new list from the grid that forms each cell from each
+        column of the original gird, then i count how many times 1 in
+        the new list rows and same thing with the grid, then i add both
+        and multiply by 2
+    """
+    n_row = len(grid)
+    n_col = len(grid[0])
+    col_count = 0
+    row_count = 0
+
+    # maximum none zero
+    # check for the row that has more than none zero character
+    # creates a new list that forms from each column in grid
+    max_nz = max(sum(grid[r][c] for r in range(n_row)) for c in range(n_col))
+    new_list = [[0 for _ in range(max_nz)] for _ in range(n_col)]
+    for col in range(n_col):
+        non_zero_count = sum(grid[row][col] for row in range(n_row))
+        for i in range(non_zero_count):
+            new_list[col][i] = 1
+
+    for rows in grid:
+        if isinstance(rows, list):
+            if 1 in rows:
+                row_count += 1
+
+    for rows in new_list:
+        if isinstance(rows, list):
+            if 1 in rows:
+                col_count += 1
+    parameter = 2 * (row_count + col_count)
+    return parameter
